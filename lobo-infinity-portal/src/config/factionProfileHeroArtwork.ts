494a4e0ce7cbde6f5a66d16fb16a6ff873879136
Jake Strangeway <lobo0705infinity@gmail.com>
2026-09-06 15:52:00 -0400

    Update Faction Profile artwork

--- lobo-infinity-portal/src/config/factionProfileHeroArtwork.ts
+++ lobo-infinity-portal/src/config/factionProfileHeroArtwork.ts
@@ -5,7 +5,7 @@ export type FactionProfileHeroArtwork = {
 }
 
 const FACTION_PROFILE_HERO_BASE_PATH = '/assets/faction-profile-heroes/'
-const FACTION_PROFILE_HERO_VERSION = '3f3ddf7'
+const FACTION_PROFILE_HERO_VERSION = '20260906-faction-profiles'
 
 const canonicalFactionProfileHeroFiles = {
   ALEPH: 'aleph.png',
