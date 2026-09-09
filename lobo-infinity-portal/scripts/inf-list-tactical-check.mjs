527fb29d37c0818adc7aa20e05d3e2ca513e9e04
lobo0705infinity-sketch <lobo0705infinity@gmail.com>
2026-09-09 13:06:05 -0400

    Test Ajax ambiguous Burst recovery

--- lobo-infinity-portal/scripts/inf-list-tactical-check.mjs
+++ lobo-infinity-portal/scripts/inf-list-tactical-check.mjs
@@ -102,6 +102,7 @@ const ajaxProfiles = buildSubmittedProfiles({
   armyCode: ajaxCode,
   cards: [{ combinedId: '702-1594-1-3-1', bs: 13, profileName: 'AJAX (Forward Deployment [+8])', skills: ['BS Attack (+1B)'], weapons: ['MULTI Rifle', 'AP Heavy Pistol'] }],
   metadata: { weapons: [{ id: 9001, name: 'MULTI Rifle', mode: 'Burst Mode', type: 'WEAPON', burst: 3 }, { id: 9001, name: 'MULTI Rifle', mode: 'Anti-materiel Mode', type: 'WEAPON', burst: 1 }] },
+  officialPayloads: [{ units: [{ id: 1594, isc: 'AJAX', profileGroups: [{ id: 1, profiles: [{ id: 1, bs: 13, weapons: [{ id: 9001 }] }], options: [{ id: 3, name: 'AJAX (Forward Deployment [+8])' }] }] }] }],
 })
 const ajaxAnalysis = classifyTacticalBrief(ajaxProfiles)
 assert.equal(ajaxAnalysis.categories.apex.some((item) => item.combinedId === '702-1594-1-3-1'), false)
